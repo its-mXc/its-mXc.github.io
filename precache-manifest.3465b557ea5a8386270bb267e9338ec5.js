f43a417ae0ac8b437854e93acdb692de0d0969b0
Uday Nayak <udaynayakgkv@gmail.com>
2020-06-17 16:40:35 +0530

    mobile view

--- precache-manifest.3465b557ea5a8386270bb267e9338ec5.js
+++ precache-manifest.3465b557ea5a8386270bb267e9338ec5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "d72a1977c062942f2b15",
+    "revision": "b9e53f445339caa29752",
     "url": "/static/css/main.daa8d2d7.chunk.css"
   },
   {
-    "revision": "d72a1977c062942f2b15",
-    "url": "/static/js/main.d72a1977.chunk.js"
+    "revision": "b9e53f445339caa29752",
+    "url": "/static/js/main.b9e53f44.chunk.js"
   },
   {
     "revision": "1922f81c0709865968ce",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "19a1fcbbae48bf44b64ffcd72aa04cfd",
+    "revision": "503476a2ac4cbc2f04e98d50e347e92e",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b220e41613ad9355842aef5f5ac64a64.js"
+  "/precache-manifest.3465b557ea5a8386270bb267e9338ec5.js"
 );
 
 workbox.clientsClaim();
